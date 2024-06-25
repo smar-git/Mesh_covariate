30654d70309331589b9a1e89610d01b74c080278
Virginia Morera-Pujol <morera.virginia@gmail.com>
2024-06-25 12:48:03 +0100

    Update test_square.R

--- test_square.R
+++ test_square.R
@@ -1,4 +1,3 @@
-
 #' Here we simulate three PP using  a LGCP where the log intensity
 #' depends on only an intercept and a covariate
 #' 
@@ -18,9 +17,8 @@ library(INLA)
 library(sf)
 library(scico)
 library(tidyterra)
+library(scico)
 library(terra)
-library(fmesher)
-
 rm(list = ls())
 
 set.seed(124545)
@@ -57,132 +55,91 @@ cov2 <- rast(nrows = max_dom/res, ncols=max_dom/res, xmin=0, xmax=max_dom, ymin 
 cov3 <- rast(nrows = max_dom/res, ncols=max_dom/res, xmin=0, xmax=max_dom, ymin = 0, ymax = max_dom)
 
 
-# create meshes and int points -----------------------------------------------------------
+## create meshes and int points -----------------------------------------------------------
 meshes = list()
 meshes[[1]] =  fm_mesh_2d_inla(boundary = poly,
                                max.edge = c(95,100))
 
 
 ggplot() + gg(meshes[[1]]) + coord_equal()
-<<<<<<< HEAD
-
-n_meshes = 4
-
-for(i in 2:n_meshes) {
-=======
 n_meshes = 5
 for(i in 2:n_meshes)
->>>>>>> b057fa3ad3e601c23af2bdec3113727435621be9
   meshes[[i]] = fmesher:::fm_subdivide(meshes[[i-1]])
-  
-  
-  sapply(meshes, function(x)x$n) %>% plot()
 
-} 
 
-p1 <- ggplot() + gg(meshes[[1]]) + coord_equal()
-p2 <- ggplot() + gg(meshes[[2]]) + coord_equal()
-p3 <- ggplot() + gg(meshes[[3]]) + coord_equal()
-p4 <- ggplot() + gg(meshes[[4]]) + coord_equal()
+sapply(meshes, function(x)x$n) %>% plot()
 
-gridExtra::grid.arrange(p1, p2, p3, p4, nrow = 2)
+ggplot() + gg(meshes[[n_meshes]]) + coord_equal()
 
 int_points = list()
-
-for(i in 1:n_meshes){
+for(i in 1:n_meshes)
+{
   print(paste("compute int. point  for mesh", i))
   int_points[[i]] = fm_int(meshes[[i]], samplers = poly)
 }
 
 
+
 # simulate a covariate ----------------------------------------------------
 
-
-sim_mesh <- fm_mesh_2d_inla(boundary = poly, 
-                            max.edge = c(5, 10))
-
+sim_mesh <- inla.mesh.2d(loc.domain = poly, 
+                         max.edge = c(5, 10))
 sim_matern <- inla.spde2.pcmatern(sim_mesh,
                                   prior.sigma = c(1, 0.5),
                                   prior.range = c(1, 0.5))
 A = inla.spde.make.A(mesh = sim_mesh, loc  = crds(cov1))
 
-## cov 1, short range
+
 range = 8
 sigma = 1
-
 Q <- inla.spde2.precision(spde = sim_matern,
                           theta = c(log(range),log(sigma)))
-
 samp1 <- inla.qsample(n = 2,
                      Q = Q,
                      mu = rep(0,dim(Q)[1]))
-
 val =  as.vector((A%*%samp1[,1]))
-
 #val =   ifelse(val<1.5,0,val)
-
 values(cov1) = val #- mean(val[val>0])
-
 names(cov1) =  "val"
 
-ggplot() + 
-  geom_spatraster(data = cov1) + 
-  scale_fill_scico(palette = 'imola', direction = -1) +
-  gg(meshes[[1]]) + 
-  coord_equal()
+ggplot() + geom_spatraster(data = cov1) + scale_fill_scico( direction = -1) +
+  gg(meshes[[1]])+ coord_equal()
 
-## cov 2, medium range
+
 range = 40
 sigma = 1
-
 Q <- inla.spde2.precision(spde = sim_matern,
                           theta = c(log(range),log(sigma)))
 samp2 <- inla.qsample(n = 2,
                       Q = Q,
                       mu = rep(0,dim(Q)[1]))
-
 val =  as.vector((A%*%samp2[,1]))
-
 #val = ifelse(val<1,0,val)
-
 values(cov2) = val# - mean(val[val>0])
-
 names(cov2) =  "val"
+ggplot() + geom_spatraster(data = cov2) + scale_fill_scico( direction = -1) +
+  gg(meshes[[1]]) + coord_equal()
 
-ggplot() + 
-  geom_spatraster(data = cov2) + 
-  scale_fill_scico(palette = 'imola', direction = -1) +
-  gg(meshes[[1]]) + 
-  coord_equal()
 
-## cov 3, long range
 range = 600
 sigma = 1
-
 Q <- inla.spde2.precision(spde = sim_matern,
                           theta = c(log(range),log(sigma)))
-
 samp3 <- inla.qsample(n = 2,
                      Q = Q,
                      mu = rep(0,dim(Q)[1]))
-
 val =  as.vector((A%*%samp3[,1]))
-
 values(cov3) = val -  mean(val)
-
 names(cov3) =  "val"
 
-ggplot() + 
-  geom_spatraster(data = cov3) + 
-  scale_fill_scico(palette = 'imola', direction = -1) +
-  gg(meshes[[1]]) + 
-  coord_equal()
+ggplot() + geom_spatraster(data = cov3) + scale_fill_scico( direction = -1) +
+  gg(meshes[[1]])+ coord_equal()
 
 # simulate a point process ------------------------------------------------
 
 
-simulate_PP = function(loglambda) {
-  
+simulate_PP = function(loglambda)
+{
   wmax <- max(values(loglambda))
   Npoints <- rpois(1, lambda = max_dom^2 * exp(wmax))
   pointsX <- runif(n = Npoints, min = 0, max = max_dom)
@@ -221,13 +178,12 @@ ggplot() + geom_spatraster(data = cov1)+
   theme_maps + 
   scale_fill_scico(direction = -1) + 
   theme(legend.position = "none") +
-
 ggplot() + geom_spatraster(data = cov2)+
   geom_sf(data = points2, size = 0.3) +
   theme_maps + 
   scale_fill_scico(direction = -1) + 
   theme(legend.position = "none") +
-
+  
 ggplot() + geom_spatraster(data = cov3)+
   geom_sf(data = points3, size = 0.3) +
   theme_maps + 
@@ -236,29 +192,24 @@ ggplot() + geom_spatraster(data = cov3)+
   
   plot_layout(ncol = 2)
 
-<<<<<<< HEAD
-
-if(0) {
-=======
 ggsave(paste(plot_save_dir,"simulatedPP.png",sep = ""))
 if(0)
   {
->>>>>>> b057fa3ad3e601c23af2bdec3113727435621be9
   data.frame( terra::extract(cov1, rbind(st_coordinates(points1), crds(cov1)))) %>% 
-    mutate(p = c(rep(1, nrow(points1)), rep(0,dim(crds(cov1))[1]))) %>%
-    group_by(p) %>%
-    summarise(m = mean(val))
-  
-  data.frame( terra::extract(cov2, rbind(st_coordinates(points2), crds(cov2)))) %>% 
-    mutate(p = c(rep(1, nrow(points2)), rep(0,dim(crds(cov2))[1]))) %>%
-    group_by(p) %>%
-    summarise(m = mean(val))
-  
-  
-  data.frame( terra::extract(cov3, rbind(st_coordinates(points3), crds(cov3)))) %>% 
-    mutate(p = c(rep(1, nrow(points3)), rep(0,dim(crds(cov3))[1]))) %>%
-    group_by(p) %>%
-    summarise(m = mean(val))
+  mutate(p = c(rep(1, nrow(points1)), rep(0,dim(crds(cov1))[1]))) %>%
+  group_by(p) %>%
+  summarise(m = mean(val))
+
+data.frame( terra::extract(cov2, rbind(st_coordinates(points2), crds(cov2)))) %>% 
+  mutate(p = c(rep(1, nrow(points2)), rep(0,dim(crds(cov2))[1]))) %>%
+  group_by(p) %>%
+  summarise(m = mean(val))
+
+
+data.frame( terra::extract(cov3, rbind(st_coordinates(points3), crds(cov3)))) %>% 
+  mutate(p = c(rep(1, nrow(points3)), rep(0,dim(crds(cov3))[1]))) %>%
+  group_by(p) %>%
+  summarise(m = mean(val))
 }
 
 
@@ -277,15 +228,18 @@ bru_options_set(bru_verbose = 2)
 
 
 ## MESH 1  --------------------------------------------------
+ 
+
 
 models1 = list()
 models2 = list()
 models3 = list()
 
+
 cmp1 = ~ Intercept(1, model = "linear", prec.linear = 0.01)
 
-for(i in 1:n_meshes) {
-  
+for(i in 1:n_meshes)
+{
   print(paste("MESH 1", " Int points ",i))
   lik = like(geometry ~ .,
              data = st_as_sf(points1),
@@ -323,6 +277,8 @@ for(i in 1:n_meshes) {
   models3[[i]] = bru(update.formula(cmp1, .~ . + 
                                       cov(cov3, model = "linear")), 
                      lik)
+  
+  
 }
 
 
@@ -376,4 +332,3 @@ rbind(aa1, aa2, aa3 ) %>%
   geom_vline(aes(xintercept = true), linetype = "dashed") + 
   facet_grid(cov~ ., scales = "free") +
   ggtitle("covariate")
-
